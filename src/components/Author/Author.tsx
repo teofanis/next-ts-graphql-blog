82d91b0d5c3b54f049f06c288b479267e6d62a2a
Teofanis Papadopulos <teos_97@hotmail.com>
2021-11-22 23:32:18 +0000

    Author

--- src/components/Author/Author.tsx
+++ src/components/Author/Author.tsx
@@ -1,4 +1,5 @@
 import { Author } from '@interfaces/app.interfaces'
+import Image from 'next/image'
 import React from 'react'
 
 interface AuthorProps {
@@ -6,8 +7,18 @@ interface AuthorProps {
 }
 const Author = ({ author }: AuthorProps) => {
   return (
-    <div>
-      <h1>Author : {author.name}</h1>
+    <div className="text-center mt-20 mb-8 p-12 relative rounded-lg bg-black bg-opacity-20">
+      <div className="absolute left-0 right-2 -top-14">
+        <Image
+          alt={author.name}
+          className="align-middle rounded-full"
+          src={author.photo.url}
+          width={100}
+          height={100}
+        />
+      </div>
+      <h3 className="text-white my-4 text-xl font-bold">{author.name}</h3>
+      <p className="text-white text-lg">{author.bio}</p>
     </div>
   )
 }
