e1980ad3c7cae23550feaab847e5158d8cf8537a
Teos <47872542+teofanis@users.noreply.github.com>
2021-12-14 16:17:28 +0200

    _document exports a function instead of class (#6)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,18 +1,16 @@
-import Document, { Head, Html, Main, NextScript } from 'next/document'
+import { Head, Html, Main, NextScript } from 'next/document'
 import React from 'react'
 
-class MyDocument extends Document {
-  render() {
-    return (
-      <Html lang="en">
-        <Head />
-        <body className="dark:from-gray-800 dark:via-violet-900 dark:to-gray-600 dark:bg-gradient-to-r from-purple-500 via-indigo-500 to-purple-400 bg-gradient-to-r">
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
+const MyDocument = () => {
+  return (
+    <Html lang="en">
+      <Head />
+      <body className="dark:from-gray-800 dark:via-violet-900 dark:to-gray-600 dark:bg-gradient-to-r from-purple-500 via-indigo-500 to-purple-400 bg-gradient-to-r">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
 }
 
 export default MyDocument
