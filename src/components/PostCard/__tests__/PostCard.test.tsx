36d9c693e618fad55dcd1e8504b52636b78cc5e1
Teofanis Papadopulos <teos_97@hotmail.com>
2021-12-28 21:32:51 +0000

    Post Card Test

--- src/components/PostCard/__tests__/PostCard.test.tsx
+++ src/components/PostCard/__tests__/PostCard.test.tsx
@@ -1,13 +1,13 @@
-import { FeaturedPostCard } from '@components/index'
+import { PostCard } from '@components/index'
 import { Post } from '@interfaces/app.interfaces'
 import { render } from '@utils/test-utils'
 import { post } from '../__fixtures__/post.json'
 
 const postMock = post as unknown as Post
 
-describe('Featured Post Card Test', () => {
+describe('Post Card Test', () => {
   it('should render properly', () => {
-    const { container } = render(<FeaturedPostCard post={postMock} />)
+    const { container } = render(<PostCard post={postMock} />)
     expect(container).toMatchSnapshot()
   })
 })
